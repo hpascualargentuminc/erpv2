6dac89d74dec7893a574222cc2944a14443e52a0
hpascual <34423185+hpascual@users.noreply.github.com>
2022-06-20 13:40:34 -0400

    Try except

--- adecuaciones_argentum/models/project_task.py
+++ adecuaciones_argentum/models/project_task.py
@@ -8,13 +8,16 @@ class Task(models.Model):
     _inherit = 'project.task'
 
     invoice_id = fields.Many2one('account.move', string='Factura', help='Factura asociada a la Tarea/Hito')
-    parent_partner_id = fields.Many2one('res.partner', compute="_compute_parent_partner_id", store=True)
+    parent_partner_id = fields.Many2one('res.partner', compute="_compute_parent_partner_id")
     
     @api.onchange('partner_id')
     def _compute_parent_partner_id(self):
         for record in self:    
-            if record.partner_id.company_type == 'company':
-                record.parent_partner_id = record.partner_id
-            elif record.partner_id.company_type == 'person':
-                record.parent_partner_id = record.partner_id.parent_id if record.partner_id.parent_id else record.partner_id
+            try:
+                if record.partner_id.company_type == 'company':
+                    self.parent_partner_id = self.partner_id
+                else:
+                    self.parent_partner_id = self.partner_id.parent_id if self.partner_id.parent_id else self.partner_id
+            except:
+                pass
                 
