2a2df3d688f3953eea2a9d29fe8aeece7edb14d1
hpascual <34423185+hpascual@users.noreply.github.com>
2022-06-20 16:00:57 -0400

    logger

--- adecuaciones_argentum/models/project_task.py
+++ adecuaciones_argentum/models/project_task.py
@@ -28,4 +28,5 @@ class Task(models.Model):
     @api.onchange('date_deadline')
     def _update_invoice_date(self):
         for record in self:
-            record.invoice_id.invoice_date = record.date_deadline
+            resultado = record.invoice_id.write({'invoice_date': record.date_deadline})
+            _logger.info(f"RESULTADO: {resultado}")
