83b67fcbe96fa797defdcbbe64a3c97cda89d68f
hpascual <34423185+hpascual@users.noreply.github.com>
2022-06-20 12:31:00 -0400

    Fix domain

--- adecuaciones_argentum/models/project_task.py
+++ adecuaciones_argentum/models/project_task.py
@@ -8,3 +8,12 @@ class Task(models.Model):
     _inherit = 'project.task'
 
     invoice_id = fields.Many2one('account.move', string='Factura', help='Factura asociada a la Tarea/Hito')
+    parent_partner_id = fields.Many2one('res.partner', compute="_compute_parent_partner_id")
+    
+    @api.onchange('partner_id')
+    def _compute_parent_partner_id(self):
+        if self.partner_id:
+            if self.partner_id.parent_id != False:
+                self.parent_partner_id = self.partner_id.parent_id
+            else:
+                self.parent_partner_id = self.partner_id
