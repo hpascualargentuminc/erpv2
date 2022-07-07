a478242e036ef3cc86fd0e31e985cae2123bbd70
hpascual <34423185+hpascual@users.noreply.github.com>
2022-07-07 13:41:06 -0400

    Se quita el orden

--- adecuaciones_argentum/models/crm_lead.py
+++ adecuaciones_argentum/models/crm_lead.py
@@ -13,7 +13,7 @@ class Lead(models.Model):
     @api.onchange('date_deadline','expected_revenue')
     def _update_data_on_opportunity(self):
         for opp in self:
-            for order in opp.order_ids.sorted(key=lambda r: r.id):
+            for order in opp.order_ids:
                 if order.state not in ('cancel') and opp.date_deadline and order.payment_term_id:
                     opp.first_invoice_date = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=opp.date_deadline)[0][0]
                     opp.first_invoice_amount = opp.expected_revenue * (order.first_invoice_perc/100)
