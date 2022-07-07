4f831e282a35eed5a995cb2deb332b95664b300a
hpascual <34423185+hpascual@users.noreply.github.com>
2022-07-07 14:27:57 -0400

    a ver ahora

--- adecuaciones_argentum/models/crm_lead.py
+++ adecuaciones_argentum/models/crm_lead.py
@@ -12,18 +12,19 @@ class Lead(models.Model):
     
     @api.onchange('date_deadline')
     def _update_data_on_opportunity(self):
-        # for opp in self:
-        _logger.info(f"Order IDs: {self.order_ids}")
-        for order in self.order_ids:
-            _logger.info(f"Order state: {order.state}")
-            _logger.info(f"Opp Datedeadline: {self.date_deadline}")
-            _logger.info(f"Order Payment Term: {order.payment_term_id}")
-            if order.state not in ('cancel') and self.date_deadline and order.payment_term_id:
-                first_invoice_date = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=self.date_deadline)[0][0]
-                first_invoice_amount = self.expected_revenue * (order.first_invoice_perc/100)
-                _logger.info(f"first_invoice_date: {first_invoice_date} | first_invoice_amount: {first_invoice_amount}")
-                self.first_invoice_date = first_invoice_date
-                self.first_invoice_amount = first_invoice_amount
-                #opp.sudo().write({'first_invoice_date': first_invoice_date, 'first_invoice_amount': first_invoice_amount})
-                # break
+        for opt in self:
+            _logger.info(f"Order IDs: {self.order_ids}")
+            for order in opt.order_ids:
+                _logger.info(f"Order state: {order.state}")
+                _logger.info(f"Opp Datedeadline: {opt.date_deadline}")
+                _logger.info(f"Order Payment Term: {order.payment_term_id}")
+                if order.state not in ('cancel') and opt.date_deadline and order.payment_term_id:
+                    first_invoice_date = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=opt.date_deadline)[0][0]
+                    first_invoice_amount = opt.expected_revenue * (order.first_invoice_perc/100)
+                    _logger.info(f"first_invoice_date: {first_invoice_date} | first_invoice_amount: {first_invoice_amount}")
+                    opt.first_invoice_date = first_invoice_date
+                    opt.first_invoice_amount = first_invoice_amount
+                    _logger.info(f"UPD: first_invoice_date: {opt.first_invoice_date} | first_invoice_amount: {opt.first_invoice_amount}")
+                    #opp.sudo().write({'first_invoice_date': first_invoice_date, 'first_invoice_amount': first_invoice_amount})
+                    # break
             
