e63040ae67e32386acf53b2b5721afd2027043d2
hpascual <34423185+hpascual@users.noreply.github.com>
2022-07-07 14:43:48 -0400

    Ajuste

--- adecuaciones_argentum/models/crm_lead.py
+++ adecuaciones_argentum/models/crm_lead.py
@@ -13,6 +13,8 @@ class Lead(models.Model):
     @api.onchange('date_deadline')
     def _update_data_on_opportunity(self):
         for opt in self:
+            fid = opt.first_invoice_date
+            fia = opt.first_invoice_amount
             _logger.info(f"Order IDs: {self.order_ids}")
             for order in opt.order_ids:
                 _logger.info(f"Order state: {order.state}")
@@ -22,9 +24,10 @@ class Lead(models.Model):
                     fid = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=opt.date_deadline)[0][0]
                     fia = opt.expected_revenue * (order.first_invoice_perc/100)
                     _logger.info(f"first_invoice_date: {fid} | first_invoice_amount: {fia}")
-                    opt.first_invoice_date = fid
-                    opt.first_invoice_amount = fia
-                    _logger.info(f"UPD: first_invoice_date: {opt.first_invoice_date} | first_invoice_amount: {opt.first_invoice_amount}")
+        
+            opt.first_invoice_date = fid
+            opt.first_invoice_amount = fia
+            _logger.info(f"UPD: first_invoice_date: {opt.first_invoice_date} | first_invoice_amount: {opt.first_invoice_amount}")
                     #opp.sudo().write({'first_invoice_date': first_invoice_date, 'first_invoice_amount': first_invoice_amount})
                     # break
             
