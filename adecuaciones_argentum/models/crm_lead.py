fe8acffd28c1b693409fce1e10a6845f21660fd7
hpascual <34423185+hpascual@users.noreply.github.com>
2022-07-07 15:02:53 -0400

    log log log

--- adecuaciones_argentum/models/crm_lead.py
+++ adecuaciones_argentum/models/crm_lead.py
@@ -12,22 +12,23 @@ class Lead(models.Model):
     
     @api.onchange('date_deadline')
     def _update_data_opportunity(self):
-        for opt in self:
-            fid = opt.first_invoice_date
-            fia = opt.first_invoice_amount
+        for record in self:
+            fid = record.first_invoice_date
+            fia = record.first_invoice_amount
             _logger.info(f"Order IDs: {self.order_ids}")
             for order in opt.order_ids:
                 _logger.info(f"Order state: {order.state}")
-                _logger.info(f"Opp Datedeadline: {opt.date_deadline}")
+                _logger.info(f"Opp Datedeadline: {record.date_deadline}")
                 _logger.info(f"Order Payment Term: {order.payment_term_id}")
-                if order.state not in ('cancel') and opt.date_deadline and order.payment_term_id:
-                    fid = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=opt.date_deadline)[0][0]
-                    fia = opt.expected_revenue * (order.first_invoice_perc/100)
+                if order.state not in ('cancel') and record.date_deadline and order.payment_term_id:
+                    fid = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=record.date_deadline)[0][0]
+                    fia = record.expected_revenue * (order.first_invoice_perc/100)
                     _logger.info(f"first_invoice_date: {fid} | first_invoice_amount: {fia}")
         
-            #opt.first_invoice_date = fid
-            #opt.first_invoice_amount = fia
-            _logger.info(f"UPD: first_invoice_date: {opt.first_invoice_date} | first_invoice_amount: {opt.first_invoice_amount}")
-            opt.sudo().update({'first_invoice_date': fid, 'first_invoice_amount': fia})
+            record.first_invoice_date = fid
+            record.first_invoice_amount = fia
+            _logger.info(f"UPD: first_invoice_date: {fid} | first_invoice_amount: {fia}")
+            _logger.info(f"UPD 2: first_invoice_date: {record.first_invoice_date} | first_invoice_amount: {record.first_invoice_amount}")
+            #record.sudo().update({'first_invoice_date': fid, 'first_invoice_amount': fia})
             # break
             
