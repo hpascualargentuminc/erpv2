4ce12a447685ab1d3a6c5db5dd68411942cfc7fc
argentum-odoo-sh <102610231+argentum-odoo-sh@users.noreply.github.com>
2022-07-07 12:05:11 -0400

    Se agrega import

--- adecuaciones_argentum/models/sale_order.py
+++ adecuaciones_argentum/models/sale_order.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from odoo.exceptions import Warning
+from odoo.exceptions import Warning, ValidationError
 import logging
 
 _logger = logging.getLogger(__name__)
