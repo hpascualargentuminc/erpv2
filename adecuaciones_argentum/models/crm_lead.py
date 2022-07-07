46f5e3db15655290b4e1a52e75164888c85cf13c
hpascual <34423185+hpascual@users.noreply.github.com>
2022-07-07 15:16:18 -0400

    Ajuste final

--- adecuaciones_argentum/models/crm_lead.py
+++ adecuaciones_argentum/models/crm_lead.py
@@ -13,22 +13,12 @@ class Lead(models.Model):
     @api.onchange('date_deadline')
     def _update_data_opportunity(self):
         for record in self:
-            fid = record.first_invoice_date
-            fia = record.first_invoice_amount
-            _logger.info(f"Order IDs: {self.order_ids}")
+            invoice_date = record.first_invoice_date
+            invoice_amount = record.first_invoice_amount
             for order in record.order_ids:
-                _logger.info(f"Order state: {order.state}")
-                _logger.info(f"Opp Datedeadline: {record.date_deadline}")
-                _logger.info(f"Order Payment Term: {order.payment_term_id}")
                 if order.state not in ('cancel') and record.date_deadline and order.payment_term_id:
-                    fid = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=record.date_deadline)[0][0]
-                    fia = record.expected_revenue * (order.first_invoice_perc/100)
-                    _logger.info(f"first_invoice_date: {fid} | first_invoice_amount: {fia}")
+                    invoice_date = order.payment_term_id.compute(value=order.base_amount_untaxed, date_ref=record.date_deadline)[0][0]
+                    invoice_amount = record.expected_revenue * (order.first_invoice_perc/100)
         
-            record.first_invoice_date = fid
-            record.first_invoice_amount = fia
-            _logger.info(f"UPD: first_invoice_date: {fid} | first_invoice_amount: {fia}")
-            _logger.info(f"UPD 2: first_invoice_date: {record.first_invoice_date} | first_invoice_amount: {record.first_invoice_amount}")
-            #record.sudo().update({'first_invoice_date': fid, 'first_invoice_amount': fia})
-            # break
-            
+            record.first_invoice_date = invoice_date
+            record.first_invoice_amount = invoice_amount     
