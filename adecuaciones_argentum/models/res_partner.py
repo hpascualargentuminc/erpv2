c25632ac00388796eb02b127b4ff95f51141f98e
hpascual <34423185+hpascual@users.noreply.github.com>
2022-09-20 14:35:46 -0400

    Campo Notes_RRHH

---
+++ adecuaciones_argentum/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    rrhh_notes = fields.Html(string='Notas RRHH')
